abdf8b47489f843a7f8f38e6dce49ee50a9be386
Binas <Binas@L-MDBI-24.staring.local>
2015-10-27 16:12:11 +0100

    al;sdfk

--- src/Test1.java
+++ src/Test1.java
@@ -3,21 +3,22 @@ import java.util.Date;
 public class Test1 {
 
 	public static void main(String[] args) {
-		int tijdBinnenkomst = 9;
+		public static void log(String s) {
+			System.out.println(s);
+		}
+
+		Date date = new Date(2015,10,27,9,14,0);
+		if (date.getHours() >= 9 && date.getMinutes() > 15) {
+			log("oeps");
+		}
 		if (tijdBinnenkomst <= 10) {
 			log("Aanwezig");
 		} else if (tijdBinnenkomst < 9.05) {
 			log("Te laat");
 		} else {
-			log("Nt aanwezig");
+			log("Niet aanwezig");
 		}
-		Date date = new Date(2015,10,27,9,14,0);
-		if (date.getHours() >= 9 && date.getMinutes() > 15) {
-			log("oeps");
-		}
+		
+	
 	}
-	public static void log(String s) {
-		System.out.println(s);
-	}
-
 }
