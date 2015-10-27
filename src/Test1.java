dea2ba17879c1d156235ab59a6326e3aee9e284c
Binas <Binas@L-MDBI-24.staring.local>
2015-10-27 16:14:25 +0100

    bla

--- src/Test1.java
+++ src/Test1.java
@@ -1,11 +1,11 @@
 import java.util.Date;
 
 public class Test1 {
+	public static void log(String s) {
+		System.out.println(s);
+	}
 
 	public static void main(String[] args) {
-		public static void log(String s) {
-			System.out.println(s);
-		}
 
 		Date date = new Date(2015,10,27,9,14,0);
 		if (date.getHours() >= 9 && date.getMinutes() > 15) {
