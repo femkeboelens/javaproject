8a3b9729984aabc38402dc2023f05ab19bd25aac
ramon <ramononis@hotmail.com>
2015-10-27 16:16:22 +0100

    hoi

--- src/Test1.java
+++ src/Test1.java
@@ -11,13 +11,7 @@ public class Test1 {
 		if (date.getHours() >= 9 && date.getMinutes() > 15) {
 			log("oeps");
 		}
-		if (tijdBinnenkomst <= 10) {
-			log("Aanwezig");
-		} else if (tijdBinnenkomst < 9.05) {
-			log("Te laat");
-		} else {
-			log("Niet aanwezig");
-		}
+		
 		
 	
 	}
